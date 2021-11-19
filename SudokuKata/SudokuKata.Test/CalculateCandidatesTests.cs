c1bde7b4716de129b74912fb817752909bb2f4e0
Szali Szali <szalicil@gmail.com>
2021-11-19 21:14:50 +0100

    F new logic to calculate candidates

---
+++ SudokuKata/SudokuKata.Test/CalculateCandidatesTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+namespace SudokuKata.Test;
+
+internal class CalculateCandidatesTests
+{
+    [Test]
+    public void CalculateCandidatesEmptyBoard()
+    {
+        var emptyBoard = new int[9 * 9];
+        var sut = new CalculateCandidates(emptyBoard);
+
+        CandidateSet expected = new CandidateSet();
+        expected.IncludeAll();
+
+        Assert.That(sut.Get(1, 1), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void CalculateCandidatesForFilledCellAreEmpty()
+    {
+        var board = new int[9 * 9];
+        board.Set(3, 3, 3);
+        var sut = new CalculateCandidates(board);
+
+        Assert.That(sut.Get(3, 3).NumCandidates, Is.Zero);
+    }
+
+    [Test]
+    public void CalculateCandidatesForEmptyCell()
+    {
+        /*
+         ...|...|...
+         ...|.3.|...
+         ...|...|...
+         ---+---+---
+         ...|...|...
+         ...|.?.|..4
+         ...|..6|...
+         ---+---+---
+         ...|...|...
+         ...|...|...
+         ...|...|...
+         */
+
+        var board = new int[9 * 9];
+        board.Set(1, 4, 3);
+        board.Set(4, 8, 4);
+        board.Set(5, 5, 6);
+        var sut = new CalculateCandidates(board);
+
+        var expected = new CandidateSet();
+        expected.IncludeAll();
+        expected.Exclude(3);
+        expected.Exclude(4);
+        expected.Exclude(6);
+
+        Assert.That(sut.Get(4, 4), Is.EqualTo(expected));
+    }
+}
---
+++ SudokuKata/SudokuKata/CalculateCandidates.cs
@@ -0,0 +1,30 @@
+﻿namespace SudokuKata;
+
+public class CalculateCandidates
+{
+    private int[] board;
+
+    public CalculateCandidates(int[] board)
+    {
+        this.board = board;
+    }
+
+    public CandidateSet Get(int row, int column)
+    {
+        var candidates = new CandidateSet();
+        if (board.Get(row, column) == 0)
+        {
+            candidates.IncludeAll();
+            int blockRow = row / 3;
+            int blockCol = column / 3;
+
+            for (int j = 0; j < 9; j++)
+            {
+                candidates.Exclude(board.Get(row, j));
+                candidates.Exclude(board.Get(j, column));
+                candidates.Exclude(board.Get(blockRow * 3 + j / 3, blockCol * 3 + j % 3));
+            }
+        }
+        return candidates;
+    }
+}
