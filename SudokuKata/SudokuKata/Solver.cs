eeca6a30d2367b4547cce7a9bb5b0119b22c76fe
Szali Szali <szalicil@gmail.com>
2021-11-21 15:48:52 +0100

    R move function to static class

--- SudokuKata/SudokuKata/Solver.cs
+++ SudokuKata/SudokuKata/Solver.cs
@@ -63,26 +63,8 @@ internal class Solver
         }
     }
 
-    private static IEnumerable<ISolverCommand> PickCellsWithOnlyOneCandidateLeft(SolverState solverState)
-    {
-        CellLocation[] singleCandidateIndices =
-            solverState.Candidates.Zip(solverState.Board.AllLocations(), (c, l) => (Location: l, CandidatesCount: c.NumCandidates))
-                .Where(tuple => tuple.CandidatesCount == 1)
-                .Select(tuple => tuple.Location)
-                .ToArray();
-
-        if (singleCandidateIndices.Length > 0)
-        {
-            int pickSingleCandidateIndex = solverState.Rng.Next(singleCandidateIndices.Length);
-            CellLocation location = singleCandidateIndices[pickSingleCandidateIndex];
-
-            int candidate = solverState.Candidates.Get(location).SingleCandidate;
-
-            yield return new SetCellCommand(location, candidate);
-
-            Console.WriteLine("{0} can only contain {1}.", location.ShortString(), candidate);
-        }
-    }
+    private static IEnumerable<ISolverCommand> PickCellsWithOnlyOneCandidateLeft(SolverState solverState) =>
+        SolverSteps.PickCellsWithOnlyOneCandidateLeft.Solve(solverState);
 
     private static IEnumerable<ISolverCommand> TryToFindANumberWhichCanOnlyAppearInOnePlaceInARowColumnBlock(SolverState solverState)
     {
---
+++ SudokuKata/SudokuKata/SolverSteps/PickCellsWithOnlyOneCandidateLeft.cs
@@ -0,0 +1,25 @@
+﻿namespace SudokuKata.SolverSteps;
+
+public class PickCellsWithOnlyOneCandidateLeft
+{
+    public static IEnumerable<ISolverCommand> Solve(SolverState solverState)
+    {
+        CellLocation[] singleCandidateIndices =
+    solverState.Candidates.Zip(solverState.Board.AllLocations(), (c, l) => (Location: l, CandidatesCount: c.NumCandidates))
+        .Where(tuple => tuple.CandidatesCount == 1)
+        .Select(tuple => tuple.Location)
+        .ToArray();
+
+        if (singleCandidateIndices.Length > 0)
+        {
+            int pickSingleCandidateIndex = solverState.Rng.Next(singleCandidateIndices.Length);
+            CellLocation location = singleCandidateIndices[pickSingleCandidateIndex];
+
+            int candidate = solverState.Candidates.Get(location).SingleCandidate;
+
+            yield return new SetCellCommand(location, candidate);
+
+            Console.WriteLine("{0} can only contain {1}.", location.ShortString(), candidate);
+        }
+    }
+}
