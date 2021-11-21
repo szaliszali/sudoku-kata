76ae8037b0e4fe583d0e0e521a8a725a18de91df
Szali Szali <szalicil@gmail.com>
2021-11-21 19:07:01 +0100

    F!! EliminateCandidatesCommand Equals, make public
    - Equals is used for test

--- SudokuKata/SudokuKata/EliminateCandidatesCommand.cs
+++ SudokuKata/SudokuKata/EliminateCandidatesCommand.cs
@@ -1,9 +1,31 @@
 ﻿namespace SudokuKata;
 
-internal record EliminateCandidatesCommand(CellLocation Location, IReadOnlyList<int> Digits) : ISolverCommand
+public class EliminateCandidatesCommand : ISolverCommand
 {
+    CellLocation Location { get; }
+
+    IReadOnlyList<int> Digits { get; }
+
+    public EliminateCandidatesCommand(CellLocation location, IReadOnlyList<int> digits)
+    {
+        Location = location;
+        Digits = digits;
+    }
+
     void ISolverCommand.Execute(SolverState state)
     {
         state.EliminateCandidates(Location, Digits);
+    }
+
+    public override string ToString()
+    {
+        return $"Location: {Location}, Digits: {string.Join(", ", Digits)}";
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is EliminateCandidatesCommand &&
+            ((EliminateCandidatesCommand)obj).Location == Location &&
+            Enumerable.SequenceEqual(((EliminateCandidatesCommand)obj).Digits, Digits);
     }
 }
