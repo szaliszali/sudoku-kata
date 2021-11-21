43f3ed9f5501f09c1f48a707dd3384a61077d953
Szali Szali <szalicil@gmail.com>
2021-11-21 16:42:51 +0100

    F PrintMessageCommand

---
+++ SudokuKata/SudokuKata/PrintMessageCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace SudokuKata;
+
+public record PrintMessageCommand(string Message) : ISolverCommand
+{
+    void ISolverCommand.Execute(SolverState state)
+    {
+        Console.WriteLine(Message);
+    }
+}
