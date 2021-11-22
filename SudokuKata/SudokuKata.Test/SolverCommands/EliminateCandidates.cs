d0fd09bb5ba9fbcef5166505828fa6af47abec24
Szali Szali <szalicil@gmail.com>
2021-11-22 14:14:14 +0100

    t EliminateCandidatesCommand test

--- SudokuKata/SudokuKata.Test/SolverCommands/EliminateCandidates.cs
+++ SudokuKata/SudokuKata.Test/SolverCommands/EliminateCandidates.cs
@@ -1,12 +1,20 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace SudokuKata.Test.SolverCommands;
 
 internal class EliminateCandidates
 {
     [Test]
-    public void Wip()
+    public void EliminateCandidatesCommandEliminatesCandidates()
     {
-        var sut = new EliminateCandidatesCommand(new CellLocation(1, 2), new[] { 3, 4 });
+        var board = new Board();
+        var solverState = new SolverState(board, new Random());
+        solverState.RefreshCandidates();
+
+        ISolverCommand sut = new EliminateCandidatesCommand(new CellLocation(1, 2), new[] { 3, 4 });
+        sut.Execute(solverState);
+
+        Assert.That(solverState.Candidates.Get(1, 2).AllCandidates, Is.EqualTo(new[] { 1, 2, 5, 6, 7, 8, 9 }));
     }
 }
