f9f19ebd16a0c720803e7a82c4ef666f6fa5ca0d
Szali Szali <szalicil@gmail.com>
2021-11-22 19:00:48 +0100

    R LookIfTheBoardHasMultipleSolutions as ISolverStep

---
+++ SudokuKata/SudokuKata/SolverSteps/BoardHasMultipleSolutionsDetection.cs
@@ -0,0 +1,3 @@
+﻿namespace SudokuKata.SolverSteps;
+
+public record BoardHasMultipleSolutionsDetection(int index1, int index2, int digit1, int digit2);
--- SudokuKata/SudokuKata/SolverSteps/LookIfTheBoardHasMultipleSolutions.cs
+++ SudokuKata/SudokuKata/SolverSteps/LookIfTheBoardHasMultipleSolutions.cs
@@ -1,15 +1,47 @@
 ﻿namespace SudokuKata.SolverSteps;
 
-public class LookIfTheBoardHasMultipleSolutions
+public class LookIfTheBoardHasMultipleSolutions : ISolverStep<BoardHasMultipleSolutionsDetection>
 {
+    private readonly SolverState solverState;
+    private readonly int[] finalState;
+
+    private LookIfTheBoardHasMultipleSolutions(SolverState solverState, int[] finalState)
+    {
+        this.solverState = solverState;
+        this.finalState = finalState;
+    }
+
     public static IEnumerable<ISolverCommand> Solve(SolverState solverState, int[] finalState)
     {
-        // This is the last chance to do something in this iteration:
-        // If this attempt fails, board will not be entirely solved.
+        ISolverStep<BoardHasMultipleSolutionsDetection> step = new LookIfTheBoardHasMultipleSolutions(solverState, finalState);
+        return step.Act(step.Detect());
+    }
 
-        // Try to see if there are pairs of values that can be exchanged arbitrarily
-        // This happens when board has more than one valid solution
+    IEnumerable<ISolverCommand> ISolverStep<BoardHasMultipleSolutionsDetection>.Act(IReadOnlyList<BoardHasMultipleSolutionsDetection> detections)
+    {
+        if (detections.Any())
+        {
+            (int index1, int index2, int digit1, int digit2) = detections.PickOneRandomly(solverState.Rng);
+            int row1 = index1 / solverState.Board.Size;
+            int col1 = index1 % solverState.Board.Size;
+            int row2 = index2 / solverState.Board.Size;
+            int col2 = index2 % solverState.Board.Size;
 
+            string description =
+                row1 == row2 ? $"row #{row1 + 1}"
+                : col1 == col2 ? $"column #{col1 + 1}"
+                : $"block ({row1 / 3 + 1}, {col1 / 3 + 1})";
+
+            yield return new SetCellCommand(new CellLocation(row1, col1), finalState[index1]);
+            yield return new SetCellCommand(new CellLocation(row2, col2), finalState[index2]);
+
+            yield return new PrintMessageCommand(
+                $"Guessing that {digit1} and {digit2} are arbitrary in {description} (multiple solutions): Pick {finalState[index1]}->({row1 + 1}, {col1 + 1}), {finalState[index2]}->({row2 + 1}, {col2 + 1}).");
+        }
+    }
+
+    IReadOnlyList<BoardHasMultipleSolutionsDetection> ISolverStep<BoardHasMultipleSolutionsDetection>.Detect()
+    {
         Queue<(int index1, int index2, int digit1, int digit2)> candidates = new();
 
         for (int i = 0; i < finalState.Length - 1; i++)
@@ -44,7 +76,7 @@ public class LookIfTheBoardHasMultipleSolutions
         // At this point we have the lists with pairs of cells that might pick one of two digits each
         // Now we have to check whether that is really true - does the board have two solutions?
 
-        List<(int stateIndex1, int stateIndex2, int value1, int value2)> solutions = new();
+        List<BoardHasMultipleSolutionsDetection> solutions = new();
 
         while (candidates.Any())
         {
@@ -66,28 +98,10 @@ public class LookIfTheBoardHasMultipleSolutions
             if (new StackBasedFilledBoardGenerator(solverState.Rng, alternateState).HasSolution)
             {
                 // Board was solved successfully even with two digits swapped
-                solutions.Add((index1, index2, digit1, digit2));
+                solutions.Add(new(index1, index2, digit1, digit2));
             }
         }
 
-        if (solutions.Any())
-        {
-            (int index1, int index2, int digit1, int digit2) = solutions.PickOneRandomly(solverState.Rng);
-            int row1 = index1 / solverState.Board.Size;
-            int col1 = index1 % solverState.Board.Size;
-            int row2 = index2 / solverState.Board.Size;
-            int col2 = index2 % solverState.Board.Size;
-
-            string description =
-                row1 == row2 ? $"row #{row1 + 1}"
-                : col1 == col2 ? $"column #{col1 + 1}"
-                : $"block ({row1 / 3 + 1}, {col1 / 3 + 1})";
-
-            yield return new SetCellCommand(new CellLocation(row1, col1), finalState[index1]);
-            yield return new SetCellCommand(new CellLocation(row2, col2), finalState[index2]);
-
-            yield return new PrintMessageCommand(
-                $"Guessing that {digit1} and {digit2} are arbitrary in {description} (multiple solutions): Pick {finalState[index1]}->({row1 + 1}, {col1 + 1}), {finalState[index2]}->({row2 + 1}, {col2 + 1}).");
-        }
+        return solutions;
     }
 }
