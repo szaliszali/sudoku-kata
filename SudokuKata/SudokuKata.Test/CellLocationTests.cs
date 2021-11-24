0a515f3d1744d9849089eeccbc1f53bbc7c24dce
Szali Szali <szalicil@gmail.com>
2021-11-24 14:02:23 +0100

    F CellLocation.BlockIndex

---
+++ SudokuKata/SudokuKata.Test/CellLocationTests.cs
@@ -0,0 +1,10 @@
+﻿namespace SudokuKata.Test;
+
+internal class CellLocationTests
+{
+    [Test]
+    public void BlockIndex()
+    {
+        Approvals.VerifyAll(Enumerable.Range(0, 81).Select(i => new CellLocation(i / 9, i % 9).BlockIndex()), "");
+    }
+}
