6a887561ba7a05e395814a9532544c419855ba59
Szali Szali <szalicil@gmail.com>
2021-11-26 00:11:07 +0100

    t inline local var

--- SudokuKata/SudokuKata.Test/CellLocationTests.cs
+++ SudokuKata/SudokuKata.Test/CellLocationTests.cs
@@ -5,21 +5,18 @@ internal class CellLocationTests
     [Test]
     public void BlockIndex()
     {
-        var board = new Board();
-        Approvals.VerifyAll(board.AllLocations().Select(l => l.BlockIndex()), "");
+        Approvals.VerifyAll(new Board().AllLocations().Select(l => l.BlockIndex()), "");
     }
 
     [Test]
     public void BlockRow()
     {
-        var board = new Board();
-        Approvals.VerifyAll(board.AllLocations().Select(l => l.BlockRow()), "");
+        Approvals.VerifyAll(new Board().AllLocations().Select(l => l.BlockRow()), "");
     }
 
     [Test]
     public void BlockCol()
     {
-        var board = new Board();
-        Approvals.VerifyAll(board.AllLocations().Select(l => l.BlockCol()), "");
+        Approvals.VerifyAll(new Board().AllLocations().Select(l => l.BlockCol()), "");
     }
 }
