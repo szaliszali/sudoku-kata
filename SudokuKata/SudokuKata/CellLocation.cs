04c4ef8f7f2e4e22d95f53e2856d8ab14079f886
Szali Szali <szalicil@gmail.com>
2021-11-25 23:51:28 +0100

    F CellLocation.ToString

--- SudokuKata/SudokuKata/CellLocation.cs
+++ SudokuKata/SudokuKata/CellLocation.cs
@@ -5,4 +5,6 @@ public record struct CellLocation(int Row, int Column)
     public string ShortString() => $"({Row + 1}, {Column + 1})";
 
     public int BlockIndex() => 3 * (Row / 3) + Column / 3;
+
+    public override string ToString() => $"CellLocation {{ Row = {Row}, Column = {Column} }}";
 }
