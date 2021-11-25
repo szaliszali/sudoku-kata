f3ff5bbab8b130434308bc54bc9321703d341e07
Szali Szali <szalicil@gmail.com>
2021-11-25 23:55:58 +0100

    F CellLocation.BlockRow

--- SudokuKata/SudokuKata.Test/CellLocationTests.cs
+++ SudokuKata/SudokuKata.Test/CellLocationTests.cs
@@ -8,4 +8,11 @@ internal class CellLocationTests
         var board = new Board();
         Approvals.VerifyAll(Enumerable.Range(0, 81).Select(i => new CellLocation(board, i / 9, i % 9).BlockIndex()), "");
     }
+
+    [Test]
+    public void BlockRow()
+    {
+        var board = new Board();
+        Approvals.VerifyAll(Enumerable.Range(0, 81).Select(i => new CellLocation(board, i / 9, i % 9).BlockRow()), "");
+    }
 }
--- SudokuKata/SudokuKata/CellLocation.cs
+++ SudokuKata/SudokuKata/CellLocation.cs
@@ -5,6 +5,7 @@ public record struct CellLocation(Board board, int Row, int Column)
     public string ShortString() => $"({Row + 1}, {Column + 1})";
 
     public int BlockIndex() => board.BlockSize * (Row / board.BlockSize) + Column / board.BlockSize;
+    public int BlockRow() => Row / board.BlockSize;
 
     public override string ToString() => $"CellLocation {{ Row = {Row}, Column = {Column} }}";
 }
