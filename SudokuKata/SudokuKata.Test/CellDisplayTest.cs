592247cf93163a97f340ca8f6e518d1699e42352
Szali Szali <szalicil@gmail.com>
2021-11-27 14:25:00 +0100

    F CellDisplay.ToDisplay

---
+++ SudokuKata/SudokuKata.Test/CellDisplayTest.cs
@@ -0,0 +1,10 @@
+﻿namespace SudokuKata.Test;
+
+internal class CellDisplayTest
+{
+    [Test]
+    public void DigitToCharacter()
+    {
+        Approvals.VerifyAll(Enumerable.Range(0, 10).Select(CellDisplay.ToDisplay), "");
+    }
+}
---
+++ SudokuKata/SudokuKata/CellDisplay.cs
@@ -0,0 +1,11 @@
+﻿namespace SudokuKata;
+
+public static class CellDisplay
+{
+    public static char ToDisplay(int digit) => digit switch
+    {
+        0 => '.',
+        var d when 1 <= d && d <= 9 => (char)('0' + d),
+        _ => throw new ArgumentException()
+    };
+}
